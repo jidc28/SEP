ff72c698b31fc9a2e5df0eef1b3d3b5ef5e3ddbe
smaf91 <smaf91@gmail.com>
2014-05-14 13:35:45 -0430

    MODIFICADO: 	* ListarEvaluacionesEnviadas.java bug en calcular el promedio y en 	  obtener del query los rendimientos que no han sido evaluados. 	* ListarEvaluacionesPendientes.java bug en calcular el promedio. AGREGADA FUNCIONALIDAD: 	* Obtener los datos de los profesores proporcionados por la coordinacion 	  (resumenInfoCoordinacion()) en DBMS.java

--- src/java/Actions/Evaluaciones/ListarEvaluacionesPendientesGeneral.java
+++ src/java/Actions/Evaluaciones/ListarEvaluacionesPendientesGeneral.java
@@ -71,6 +71,12 @@ public class ListarEvaluacionesPendientesGeneral extends Action {
             int aplazados = evaluacion.getNota1() + evaluacion.getNota2();
             int aprobados = evaluacion.getNota3() + evaluacion.getNota4()
                     + evaluacion.getNota5();
+            
+            int total_nota = (evaluacion.getNota1() * 1) + (evaluacion.getNota2() * 2) +
+                    (evaluacion.getNota3() * 3) + (evaluacion.getNota4() * 4) +
+                    (evaluacion.getNota5() * 5);
+            float promedio = (float) total_nota / (total - evaluacion.getRetirados());
+            evaluacion.setNota_prom(promedio);
 
             /* Se obtienen los porcentajes de cada rubro y se transforman a 
              * dos decimales */
